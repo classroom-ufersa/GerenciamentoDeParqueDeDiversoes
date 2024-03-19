d5c8121805185c1e09fd5368b4ad0c76667ab4d0
Allyson-SFelix <allysonstudy2021@gmail.com>
2024-03-19 13:01:31 -0300

    Adicionado funcoes limpaBuffer e verificaEntrada alem de manutencao da funcao menuPrincipal

--- outrasFuncoes/funcoes.h
+++ outrasFuncoes/funcoes.h
@@ -1,3 +1,9 @@
 
 /*Funcao que direcionara o usuario para determinada opcao selecionada*/
 void menuPrincipal(void);
+
+/*Funcao veriricar se a entrada tem apenas 1 caractere*/
+int verificarEntradaChar(char entrada[]);
+
+/*Funcao que limpa buffer de entrada (entrada a mais que o necessario)*/
+void limpaBuffer(void);
