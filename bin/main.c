0e81fb154931ba7e34538f36553b7a42be5dc43d
Samuel <samueliagocabral@gmail.com>
2024-04-08 18:47:34 -0300

    remeção da bin

--- bin/main.c
+++
@@ -1,9 +0,0 @@
-#include "../include/atracao.h"
-
-int main(){
-    Atracoes*atracao=criaListaAtracao();
-    int valorId=0;
-    atracao = copiarArquivo(&atracao,&valorId);
-    menuPrincipal(atracao,&valorId);
-    return 0;
-}
